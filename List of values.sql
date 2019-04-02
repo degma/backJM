7efc9b1f2870b36827ff7ebe566109e68e47978a
Martin Degraf <martinplate@gmail.com>
2019-04-01 23:55:28 -0300

    cambio string db

--- List of values.sql
+++ List of values.sql
@@ -14,19 +14,20 @@ INSERT INTO public."Equipos" (nombre,activo) values ('La Lucila', true);
 INSERT INTO public."Equipos" (nombre,activo) values ('Bariloche', true);
 
 --Partidos
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (12, 1, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (4, 2, '30/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (10, 3, '06/04/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (11, 4, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (7, 5, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (14, 6, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (3, 7, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (8, 8, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (6, 9, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (5, 10, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (9, 11, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (1, 12, '22/03/2019', 1, true, 0);
-INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (13, 13, '22/03/2019', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (12, 1, '2019-03-22', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (4, 2, '2019-03-30', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (10, 3, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (11, 4, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (7, 5, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (14, 6, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (3, 7, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (8, 8, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (6, 9, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (5, 10, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (9, 11, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (1, 12, '2019-04-06', 1, true, 0);
+INSERT INTO public."Partidos" (id_equipo, nro, fecha_partido, id_torneo, activo, estado) values (13, 13, '2019-04-06', 1, true, 0);
 
+INSERT INTO public."Torneos" (nombre, activo) values ('Torneo Inicial 2019 - Libres A', true);
 
 --Queries
--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'xpsm1530',
-        database: 'jm'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
