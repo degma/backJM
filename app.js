763a4a18c6255bcc7b9e026c7d2bd1f68015194d
Martin Degraf <martinplate@gmail.com>
2019-04-01 23:25:58 -0300

    adding port

--- app.js
+++ app.js
@@ -273,6 +273,7 @@ app.put('/image', (req, res) => {
 })
 
 
-app.listen(8080, () => {
-    console.log('app is running on port 8080');
+app.listen( process.env.PORT || 8080, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+    
 })
